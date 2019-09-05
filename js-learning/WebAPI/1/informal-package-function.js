be8a2deb2c77b5c7c8b9c6ee89c3e4511dcc2357
liupeng <845457260@qq.com>
2019-09-06 01:34:38 +0800

    increase more package-function

---
+++ js-learning/WebAPI/1/informal-package-function.js
@@ -0,0 +1,41 @@
+/*
+ * @Author: apeng 
+ * @Description: 随便封装的函数 
+ * @Date: 2019-08-28 17:17:24 
+ * @Last Modified by: apeng
+ * @Last Modified time: 2019-09-06 01:18:41
+ */
+
+// 返回随机整数
+// [0, 1) * 可能值的总数 + 第一个值（即最小值）
+// 如3-5，即[3, 5]，可能值的总数为5-3+1=3个(3,4,5)，第一个值为3
+function random_Number(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+// 如在数组中 某个数到 arr.length
+// 如 [3, 5)，最大其实是取到4的
+// 此时就直接[0, 1) * (5 - 3) + 3 = [3, 5)
+function random_exceptMax(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
+
+// 在[min, max] 中随机生成不重复的 n 个数字
+// 假如说从 [1, 4]中随机生成2个不重的数字
+function select_n_number_fromMinToMax(min, max, n) {
+  let numArray = [];
+  // 返回一个随机数
+  function random_includeMax(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  while(numArray.length < n) {
+    let num = random_exceptMax(min, max)
+    // 数组中有和num相同的，则跳出循环
+    if (numArray.some(item => item === num)) {
+      continue;
+    }
+    numArray.push(num)
+  }
+  return numArray;
+}
---
+++ js-learning/informal-package-function.js
@@ -0,0 +1,19 @@
+/*
+ * @Author: apeng 
+ * @Description: 随便封装的函数 
+ * @Date: 2019-08-28 17:17:24 
+ * @Last Modified by: apeng
+ * @Last Modified time: 2019-09-05 23:59:01
+ */
+
+// 返回随机整数
+// [0, 1) * 可能值的总数 + 第一个值（即最小值）
+// 如3-5，可能值的总数为5-3+1=3个，第一个值为3
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+// 查找element
+function select(el) {
+  return document.querySelector(el)
+}
